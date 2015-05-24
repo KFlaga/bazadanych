0e8159a70c82aa769aa59e34280d05f9c2c867d4
ardr_ <anonek@gmail.com>
2015-05-24 11:19:34 +0200

    Brakujace klasy

---
+++ BazaDanych/Users/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BazaDanych
+{
+    public class User
+    {
+        public int Id { get; private set; }
+        public String Login { get; private set; }
+        private String __passwd;
+        public int Type { get; private set; }
+
+        public User(int id, String login, string passwd, int type)
+        {
+            Id = id;
+            Login = login;
+            __passwd = passwd;
+            Type = type;
+        }
+
+        public Boolean IsPasswdValid(String passwd)
+        {
+            if (__passwd.Equals(passwd))
+                return true;
+
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("({0}): {1}", Id.ToString(), Login);
+        }
+    }
+}
---
+++ BazaDanych/Users/UserList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BazaDanych.Users
+{
+    public class UserList
+    {
+        private List<User> __users;
+        public UserList()
+        {
+            __users = new List<User>();
+        }
+
+        public void ParseUser(int id, string login, string passwd, int type)
+        {
+            User user = new User(id, login, passwd, type);
+            __users.Add(user);
+            Console.WriteLine(user);
+        }
+
+        public User FindUser(String login) {
+            User user = __users.Find(u => u.Login == login);
+            return user;
+        }
+    }
+}
